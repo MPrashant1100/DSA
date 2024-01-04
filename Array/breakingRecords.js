fd16361098e833ad2dfaff3fd1f97a69cc91a505
MPrashant1100 <prashantmishra1100@gmail.com>
2024-01-04 21:58:47 +0530

    breakingRecords

---
+++ Array/breakingRecords.js
@@ -0,0 +1,22 @@
+
+const breakingRecords = (scores) => {
+    maxScore = scores[0];
+    minScore = scores[0];
+    maxCount = 0;
+    minCount = 0;
+
+    for (const score of scores) {
+        if (score > maxScore) {
+            maxScore = score;
+            maxCount++;
+        } else if (score < minScore) {
+            minScore = score;
+            minCount++;
+        }
+    }
+
+    return [maxCount, minCount];
+}
+
+const scores = [12, 24, 10, 24, 16, 25];
+console.log(breakingRecords(scores));
