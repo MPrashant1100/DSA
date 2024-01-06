2d1e7a492de71e593c2b57b97234d0ffc82eff0f
MPrashant1100 <prashantmishra1100@gmail.com>
2024-01-06 21:42:11 +0530

    .

---
+++ LinkedList/basicPractice.js
@@ -0,0 +1,10 @@
+
+class Node  {
+    constructor(data) {    // it takes data as a parameter, and when a Node object is created
+        this.data = data;  // data - to store the value
+        this.next = null   // next - to store the reference to the next node in the list.
+    }
+}
+const basicPractice = () => {
+
+}
