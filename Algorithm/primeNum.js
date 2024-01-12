815aaa858261fe06c0db2bc4fb3e6d924421cb1d
MPrashant1100 <prashantmishra1100@gmail.com>
2024-01-12 18:45:26 +0530

    isPrime

---
+++ Algorithm/primeNum.js
@@ -0,0 +1,17 @@
+// check is prime numbers or not
+
+const isPrime = (n) => {
+    if(n < 2 ) {
+        return false; 
+    }
+    for (i = 2; i < n; i++) {
+        if( n%i === 0) {
+            return false ;
+        }
+    }
+    return true;
+}
+
+console.log(isPrime(5))
+console.log(isPrime(6))
+console.log(isPrime(10))
