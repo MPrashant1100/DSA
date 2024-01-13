ba2ad48ee90dcac5272b697cf04587c72e65e1d3
MPrashant1100 <prashantmishra1100@gmail.com>
2024-01-13 21:13:38 +0530

    recursion

---
+++ Algorithm/recurciveFib.js
@@ -0,0 +1,14 @@
+const recurciveFib = (n) => {
+
+    // Base Case
+    if(n < 2) {
+        return n;
+    }
+
+    // Recurcive Function
+    return recurciveFib(n-1) + recurciveFib(n - 2);
+}
+
+
+console.log(recurciveFib(9));
+console.log(recurciveFib(11));
