995bfa9b35933a732b0910ddb93531f66d07fe9d
MPrashant1100 <prashantmishra1100@gmail.com>
2024-07-17 23:56:58 +0530

    .

--- Array/intersection.js
+++ Array/intersection.js
@@ -5,4 +5,4 @@ const intersection = (num1, num2) => {
 
 // Test cases
 console.log(intersection([1,2,2,1], [2,2,2])); 
-console.log(intersection([4,9,5], [9,4,9,8,4])); // Output: [4,9]
+console.log(intersection([4,9,5], [9,4,9,8,4]));
